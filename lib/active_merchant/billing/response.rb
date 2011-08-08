25123ffb226629c5fe03a993e59f1c51275ff5b3
Hubert Łępicki <hubert.lepicki@amberbit.com>
2011-08-08 14:45:31 +0200

    make response a bit nicer

--- lib/active_merchant/billing/response.rb
+++ lib/active_merchant/billing/response.rb
@@ -1,12 +1,12 @@
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
-  
+
     class Error < ActiveMerchantError #:nodoc:
     end
-  
+
     class Response
       attr_reader :params, :message, :test, :authorization, :avs_result, :cvv_result, :requires_3ds
-      
+
       def success?
         @success
       end
@@ -14,19 +14,19 @@ module ActiveMerchant #:nodoc:
       def test?
         @test
       end
-      
+
       def fraud_review?
         @fraud_review
       end
-      
+
       def initialize(success, message, params = {}, options = {})
         @success, @message, @params = success, message, params.stringify_keys
-        @test = options[:test] || false        
+        @test = options[:test] || false
         @authorization = options[:authorization]
         @fraud_review = options[:fraud_review]
         @avs_result = AVSResult.new(options[:avs_result]).to_hash
         @cvv_result = CVVResult.new(options[:cvv_result]).to_hash
-        @requires_3ds = options[:requires_3ds]
+        @requires_3ds = options[:requires_3ds] || false
       end
     end
   end
